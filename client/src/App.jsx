501b84595b58aaa7054dfb3cd679ea5eae1271d1
Krutik Bundela <krutikbundela2001@gmail.com>
2024-01-24 20:15:51 +0530

    Created Pages & Added Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
 import './App.css'
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
 
+  const router = createBrowserRouter([
+    { path: "/", element: <Home/> },
+    { path: "/about", element: <About/> },
+    { path: "/sign-in", element: <SignIn/> },
+    { path: "/sign-up", element: <SignUp/> },
+    { path: "/dashboard", element: <Dashboard/> },
+    { path: "/projects", element: <Projects/> },
+  ]);
+
   return (
-   <>
-   
-   </>
-  )
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App
