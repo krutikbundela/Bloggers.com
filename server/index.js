d502ffeb020a938ab1849acea7104031393646c1
Krutik Bundela <krutikbundela2001@gmail.com>
2024-01-26 14:55:57 +0530

    Connect to mongoDB And make user model

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { mongoose } from 'mongoose';
+import  dotenv  from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log("MongoDB is Connected");
+}).catch((err) =>{
+    console.log(err);
+})
+
+const app = express();
+
+app.listen(process.env.PORT, () =>{
+    console.log("Sevrer is Running", process.env.PORT);
+})
