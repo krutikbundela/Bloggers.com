aff54f630ba585ce5791243a705fd57194d5c1e7
Krutik Bundela <krutikbundela2001@gmail.com>
2024-01-26 19:49:07 +0530

    Added Error Handler

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,32 +1,30 @@
 import User from "../models/user.models.js"
 import bcryptjs from "bcryptjs"
-export const SignUp = async(req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const SignUp = async(req, res,next) => {
   try {
     const { username, email, password } = req.body;
 
     // Check if username, password, and email are provided
     if (!username || !password || !email) {
-      return res.status(400).json({ error: "All fields are required" });
+    //   return res.status(400).json({ error: "All fields are required" });
+        next(errorHandler(400,"All Fields Are Required"));
     }
-
+    
     // Check if username has at least 3 characters
     if (username.length < 3) {
-      return res
-        .status(400)
-        .json({ error: "Username must be at least 3 characters long" });
+        next(errorHandler(400, "Username must be at least 3 characters long"));
     }
-
+    
     // Check if password has at least 8 characters
     if (password.length < 8) {
-      return res
-        .status(400)
-        .json({ error: "Password must be at least 8 characters long" });
+        next(errorHandler(400, "Password must be at least 8 characters long"  ));
     }
-
+    
     // Check if email matches the regex pattern
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-      return res.status(400).json({ error: "Invalid email format" });
+        next(errorHandler(400, "Invalid email format"));
     }
 
     const hashedPassword = bcryptjs.hashSync(password,10)
@@ -41,9 +39,6 @@ export const SignUp = async(req, res) => {
 
     res.json("SignUp Successful");
 } catch (error) {
-      res.status(500).json({
-        message: error.message
-      });
-    
+      next(error)
   }
 };
--- server/index.js
+++ server/index.js
@@ -30,3 +30,22 @@ app.listen(process.env.PORT, () =>{
 
 app.use("/api/user", UserRoutes)
 app.use("/api/auth", AuthRoutes)
+
+//======================================================
+
+//Middleware For Error
+app.use((err,req,res,next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
+
+
+
+
+
