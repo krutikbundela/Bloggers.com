16cd8c1302f0421e8655a8183fb5648148385785
Krutik Bundela <krutikbundela2001@gmail.com>
2024-01-28 19:46:07 +0530

    Added Redux-Persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore , combineReducers } from "@reduxjs/toolkit";
 import { userReducer } from "./user/userSlice";
+import { persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
 
-export default configureStore({
-  reducer: {
+const rootReducer = combineReducers({
     user: userReducer,
-    
-  },
+
+})
+
+//Persist Will Add Data to local storage so on refresh the data will not be lost.
+const persistConfig = {
+    key: 'root',
+    storage,
+    version:1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware:(getDefaultMiddleware) =>
+    getDefaultMiddleware({serializableCheck:false}),
 });
+
+export const persistor = persistStore(store);
