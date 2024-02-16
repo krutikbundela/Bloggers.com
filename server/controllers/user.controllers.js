76f4589fc67442ff6afc0a57432966618316e6b1
Krutik Bundela <krutikbundela2001@gmail.com>
2024-02-16 13:34:15 +0530

    User Own Account Delete backend

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -3,7 +3,6 @@ import User from "../models/user.models.js";
 import bcryptjs from "bcryptjs";
 
 export const updateUser = async (req, res, next) => {
-
   if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, "You Are Not Allowed To Update This User"));
   }
@@ -14,24 +13,24 @@ export const updateUser = async (req, res, next) => {
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
 
- if (req.body.username) {
-   if (req.body.username.length < 7 || req.body.username.length > 20) {
-     return next(
-       errorHandler(400, "Username must be between 7 and 20 characters")
-     );
-   }
-   if (req.body.username.includes(" ")) {
-     return next(errorHandler(400, "Username cannot contain spaces"));
-   }
-   if (req.body.username !== req.body.username.toLowerCase()) {
-     return next(errorHandler(400, "Username must be lowercase"));
-   }
-   if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
-     return next(
-       errorHandler(400, "Username can only contain letters and numbers")
-     );
-   }
- }
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, "Username must be between 7 and 20 characters")
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Username must be lowercase"));
+    }
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, "Username can only contain letters and numbers")
+      );
+    }
+  }
   try {
     const updatedUser = await User.findByIdAndUpdate(
       req.params.userId,
@@ -47,6 +46,18 @@ export const updateUser = async (req, res, next) => {
     );
     const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You Are Not Allowed To Delete This User"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User Has Been Deleted")
   } catch (error) {
     next(error);
   }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,10 @@
-import express from 'express'
-import { updateUser } from '../controllers/user.controllers.js';
-import { verifyToken } from '../utils/verifyUser.js';
+import express from "express";
+import { updateUser, deleteUser } from "../controllers/user.controllers.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.put('/update/:userId',verifyToken,updateUser)
+router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
 
 export default router;
